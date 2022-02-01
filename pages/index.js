4b96165afc32231e7f6307ec3607b1b1f15cfcae
Abraham Serena <abraham.serena.lk@gmail.com>
2022-02-01 00:43:30 -0600

    Added API request for rental & sale properties

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import { baseURL, fetchApi } from "../utils/fetchApi";
+
 const Banner = ({ imgUrl, purpose, title1, title2, desc1, desc2, linkName, buttonText }) => (
-	<div className="flex flex-col flex-wrap items-center justify-center h-screen">
-		<img src={imgUrl} alt="RentMe Banner" className="w-1/2" />
+	<div className="flex flex-col flex-wrap items-center justify-center">
+		<Image src={imgUrl} alt="RentMe Banner" width={500} height={300} />
 		<div className="p-5">
 			<p className="text-gray-500 text-sm font-medium">{purpose}</p>
 			<p className="text-3xl font-bold">{title1} <br />{title2}</p>
 			<p className="text-gray-700 text-lg px-1">{desc1}<br />{desc2}</p>
-			<button className="text-xl bg-white">
+			<button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
 				<Link href={linkName}>{buttonText}</Link>
 			</button>
 		</div>
 	</div>
 )
 
-export default function Home() {
+export default function Home({ propertiesForSale, propertiesForRent }) {
+
+	console.log(propertiesForSale, propertiesForRent);
+
 	return (
 		<>
 			<Banner
@@ -28,6 +33,31 @@ export default function Home() {
 				linkName={'/search?purpose=rent'}
 				buttonText={'Rent a House'}
 			/>
+			<div className="flex flex-col flex-wrap items-center justify-center">
+
+			</div>
+			<Banner
+				imgUrl={'https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'}
+				purpose={'Buy a House'}
+				title1={'Buy a House'}
+				title2={'For Your Family'}
+				desc1={'Buy a house for your family, friends, or yourself. We have a wide variety of rental homes for you to choose from.'}
+				desc2={'We have a wide variety of rental homes for you to choose from.'}
+				linkName={'/search?purpose=buy'}
+				buttonText={'Buy a House'}
+			/>
 		</>
 	)
+}
+
+export async function getStaticProps() {
+	const propertyForSale = await fetchApi(`${baseURL}/properties/list?locationExternalIDs=5002&purpose=for-sale&page=1&hitsPerPage=6`);
+	const propertyForRent = await fetchApi(`${baseURL}/properties/list?locationExternalIDs=5002&purpose=for-rent&page=1&hitsPerPage=6`);
+
+	return {
+		props: {
+			propertiesForSale: propertyForSale?.hits,
+			propertiesForRent: propertyForRent?.hits,
+		}
+	}
 }
