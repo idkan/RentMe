e02b7816237f2853796f0e924e5ca2bac8b9e63b
Abraham Serena <abraham.serena.lk@gmail.com>
2022-02-03 01:39:20 -0600

    Changed Navbar Icons

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import { Disclosure, } from "@headlessui/react"
+import { HiOutlineHome, HiMenuAlt1, HiOutlineX } from "react-icons/hi";
 
 const navigation = [
     { name: 'Dashboard', href: '#', current: true },
@@ -25,12 +25,12 @@ const Navbar = () => (
                             {/* Mobile menu button*/}
                             <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                                 <span className="sr-only">Open main menu</span>
-                                {open ? (<XIcon className="block h-6 w-6" aria-hidden="true" />) : (<MenuIcon className="block h-6 w-6" aria-hidden="true" />)}
+                                {open ? (<HiOutlineX className="block h-6 w-6" aria-hidden="true" />) : (<HiMenuAlt1 className="block h-6 w-6" aria-hidden="true" />)}
                             </Disclosure.Button>
                         </div>
                         <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                             <div className="flex-shrink-0 flex items-center">
-                                <img className="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
+                                <HiOutlineHome color="white" className="block lg:hidden h-8 w-auto" />
                                 <img className="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow" />
                             </div>
                             <div className="hidden sm:block sm:ml-6">
