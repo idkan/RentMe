9aea66a27a6e6827bcfa1ba53dcfa4dc93b6c95e
Abraham Serena <abraham.serena.lk@gmail.com>
2022-02-06 02:46:47 -0600

    Added Not Found property list

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -3,18 +3,15 @@ import { useRouter } from "next/router";
 import { MdCancel } from "react-icons/md";
 import Image from "next/image";
 
-
 const SearchFilters = () => {
     return (
         <div className="search-filters">
             <div className="flex items-center justify-between mt-4">
                 <p className="font-medium">Filters</p>
-
                 <button className="px-4 py-2 bg-gray-100 hover:bg-gray-200 text-gray-800 text-sm font-medium rounded-md">
                     Reset Filter
                 </button>
             </div>
-
             <div>
                 <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 mt-4">
                     <select className="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
--- pages/search.js
+++ pages/search.js
@@ -4,26 +4,45 @@ import { useRouter } from "next/router";
 import { BsFilter } from "react-icons/bs";
 
 import SearchFilters from "../components/SearchFilters";
+import Property from "../components/Property";
+import NoDataImage from "../assets/images/no-data.svg";
 
 const Search = () => {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
 
     return (
-        <div className="w-full shadow p-5 rounded-lg bg-white">
-            <div className="flex justify-start items-center">
-                <button className="px-8 py-3 bg-gray-100 hover:bg-gray-200 text-gray-800 text-sm font-medium rounded-md flex justify-center items-center" onClick={() => setSearchFilters((prevFilters) => !prevFilters)}>
-                    Filters
-                    <BsFilter className="ml-2" />
-                </button>
-                <p className="text-gray-800 text-sm font-medium ml-2">
-                    Search property by Filters
-                </p>
+        <>
+            <div className="w-full shadow p-5 rounded-lg bg-white">
+                <div className="flex justify-between items-center">
+                    <button className="px-8 py-3 bg-gray-100 hover:bg-gray-200 text-gray-800 text-sm font-medium rounded-md flex justify-center items-center" onClick={() => setSearchFilters((prevFilters) => !prevFilters)}>
+                        Filters
+                        <BsFilter className="ml-2" />
+                    </button>
+                    <p className="text-gray-800 text-sm font-medium ml-2">
+                        Search property by Filters
+                    </p>
+                </div>
+                {searchFilters && (
+                    <SearchFilters />
+                )}
             </div>
-            {searchFilters && (
-                <SearchFilters />
-            )}
-        </div>
+            <div className="w-full mt-8">
+                <p className="text-3xl font-bold mb-2">
+                    Properties {router.query.purpose}
+                </p>
+                <div className="flex flex-wrap -mx-2">
+                    {[].map((property) => <Property property={property} key={property.id} />)}
+                </div>
+                {[].length === 0 && (
+                    <div className="flex flex-col justify-center items-center mt-8">
+                        <Image src={NoDataImage} width={200} height={200} alt="No Result" />
+                        <p className="text-gray-800 text-xl font-medium mt-4"> No Result Found </p>
+                    </div>
+                )}
+            </div>
+        </>
+
     );
 };
 
