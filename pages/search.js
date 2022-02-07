046b845433358f9a4f5ea243e6592a9d7e9b4527
Abraham Serena <abraham.serena.lk@gmail.com>
2022-02-07 01:34:11 -0600

    Fetch Property Data into Search Page

--- pages/search.js
+++ pages/search.js
@@ -3,11 +3,13 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 import { BsFilter } from "react-icons/bs";
 
+import { baseURL, fetchApi } from "../utils/fetchApi";
+
 import SearchFilters from "../components/SearchFilters";
 import Property from "../components/Property";
 import NoDataImage from "../assets/images/no-data.svg";
 
-const Search = () => {
+const Search = ({ properties }) => {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
 
@@ -31,10 +33,10 @@ const Search = () => {
                 <p className="text-3xl font-bold mb-2">
                     Properties {router.query.purpose}
                 </p>
-                <div className="flex flex-wrap -mx-2">
-                    {[].map((property) => <Property property={property} key={property.id} />)}
+                <div className="grid gap-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mb-8">
+                    {properties.map((property) => <Property property={property} key={property.id} />)}
                 </div>
-                {[].length === 0 && (
+                {properties.length === 0 && (
                     <div className="flex flex-col justify-center items-center mt-8">
                         <Image src={NoDataImage} width={200} height={200} alt="No Result" />
                         <p className="text-gray-800 text-xl font-medium mt-4"> No Result Found </p>
@@ -47,3 +49,25 @@ const Search = () => {
 };
 
 export default Search;
+
+export async function getServerSideProps({ query }) {
+    const purpose = query.purpose || 'for-rent';
+    const rentFrequency = query.rentFrequency || 'monthly';
+    const minPrice = query.minPrice || '0';
+    const maxPrice = query.maxPrice || '1000000';
+    const roomsMin = query.roomsMin || '0';
+    const bathsMin = query.bathsMin || '0';
+    const sort = query.sort || 'price-desc';
+    const areaMax = query.areaMax || '35000';
+    const locationExternalIDs = query.locationExternalIDs || '5002';
+    const categoryExternalID = query.categoryExternalID || '4';
+
+    const data = await fetchApi(`${baseURL}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
+
+    return {
+        props: {
+            properties: data?.hits,
+        },
+    };
+}
+
