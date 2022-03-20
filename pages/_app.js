433ca494efa01f66ca13c16fabaed23de69752ee
Abraham Serena <abraham.serena.lk@gmail.com>
2022-03-19 22:04:10 -0600

    Added Bar progress oon route change start and complete

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,22 @@ import '../styles/globals.css';
 import Head from 'next/head';
 
 import Layout from '../components/Layout.jsx';
+import NProgress from 'nprogress';
+import { Router } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+
+	NProgress.configure({ showSpinner: true });
+
+	Router.events.on('routeChangeStart', () => NProgress.start());
+
+	Router.events.on('routeChangeComplete', () => NProgress.done());
+
 	return (
 		<>
-			<Head></Head>
+			<Head>
+				<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
+			</Head>
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
